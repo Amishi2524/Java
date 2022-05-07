cc2f4f62760dbcd96f393774ef02593202770885
Amishi <78739125+Amishi2524@users.noreply.github.com>
2022-05-07 15:03:28 +0530

    Hello World Java File
    This is a java file whose purpose is to print "Hello World"

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+package javatrial;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("*****AMISHI*****\n");
+		System.out.println("Hello World!\n");
+		System.out.println("****************");
+	}
+}
